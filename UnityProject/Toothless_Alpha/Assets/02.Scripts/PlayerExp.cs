e3fdfaee1c0d8435f44a58ef917f912e97118ae9
LeeJiHun <wbsrl3532@naver.com>
2022-07-20 16:53:54 +0900

    Toothless_Alpha_Lobby_v.0.002(+NewDesign)

---
+++ UnityProject/Toothless_Alpha/Assets/02.Scripts/PlayerExp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerExp : MonoBehaviour
+{
+    public Slider Exp;
+
+    void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            Exp.value += 0.1f;
+
+            if(Exp.value >= 1.0f)
+            {
+                Exp.value = 0;
+            }
+        }
+    }
+}
